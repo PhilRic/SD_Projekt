0b330f60db7ded4a4c51cc9b154dd7b6d8b28731
PhilRic <73521963+PhilRic@users.noreply.github.com>
2023-02-21 21:24:55 +0100

    created first components

--- Smarthome-App/src/app/app.module.ts
+++ Smarthome-App/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel.component';
+import { LightComponent } from './components/light/light.component';
+import { TemperatureComponent } from './components/temperature/temperature.component';
+import { MotorComponent } from './components/motor/motor.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    LightComponent,
+    TemperatureComponent,
+    MotorComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
