5f4cdcd0c646499ec861694e987ae485b04e9a78
MaximilianHappe <63000113+MaximilianHappe@users.noreply.github.com>
2023-02-23 16:02:36 +0100

    gemeinsammer Socket

--- Smarthome-App/src/app/components/temperature/temperature.component.ts
+++ Smarthome-App/src/app/components/temperature/temperature.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WebSocketServiceService } from 'src/app/web-socket-service.service';
 @Component({
   selector: 'app-temperature',
   templateUrl: './temperature.component.html',
   styleUrls: ['./temperature.component.css']
 })
-export class TemperatureComponent implements OnInit {
+export class TemperatureComponent implements OnInit, OnDestroy{
+  tempAkt: number | undefined;
+  zielTemp: number | undefined;
+  status: string = 'unknown';
+  wasserfall_isChecked = false;
+  poolbeleuchtung_isChecked = false;
 
-  constructor() { }
+  constructor(private webSocketService: WebSocketServiceService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const host = 'ws://raspberrypi.fritz.box:1880/ws/simple';
+    this.webSocketService.connect(host);
+    this.status = this.webSocketService.status;
+    this.webSocketService.onMessage((data) => {
+      console.log('received', data);
+      if (data.topic === 'temp_akt') {
+        this.tempAkt = data.payload;
+      } 
+      if (data.topic === 'zieltemp') {
+        this.zielTemp = data.payload;
+      }
+      if (data.topic === 'wasserfall') {
+        this.wasserfall_isChecked = data.payload;
+      }
+    });
   }
 
+  ngOnDestroy() {
+    this.webSocketService.close();
+  }
+
+  sendMessage() {
+    this.webSocketService.sendMessage('test');
+  }
+
+  onToggleChange() {
+    const payload = this.wasserfall_isChecked ? 'true' : 'false';
+    this.webSocketService.sendMessage(`{"payload":"${payload}","topic":"wasserfall"}`);
+  }
 }
+
+
