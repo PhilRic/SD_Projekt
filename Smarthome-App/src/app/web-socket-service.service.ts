da4e044cd6b4a564ab532a92ad55dbe84a9b9324
MaximilianHappe <63000113+MaximilianHappe@users.noreply.github.com>
2023-02-23 16:01:03 +0100

    Auslagerung des Sockets (gemeinsammer Socket)

---
+++ Smarthome-App/src/app/web-socket-service.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketServiceService {
+  private webSocket!: WebSocket;
+  status: string = 'unknown';
+  
+
+  constructor() {}
+
+  public connect(host: string): void {
+    this.webSocket = new WebSocket(host);
+    this.webSocket.onopen = () => {
+      console.log('connected');
+      this.status = 'connected';
+    };
+    this.webSocket.onclose = () => {
+      console.log('disconnected');
+      this.status = 'not connected';
+      setTimeout(() => this.connect(host), 3000);
+    };
+  }
+
+  //weiterreichen der Empfangen Daten überall da wo der Socket verwendet wird durch Callback genau dann wann es benötigt wird
+  public onMessage(callback: (data: any) => void): void {
+    if (this.webSocket) {
+      this.webSocket.onmessage = (messageEvent) => {
+        const data = JSON.parse(messageEvent.data);
+        callback(data);
+      };
+    }
+  }
+
+  public sendMessage(message: string): void {
+    if (this.webSocket) {
+      this.webSocket.send(message);
+    }
+  }
+
+  public close(): void {
+    if (this.webSocket) {
+      this.webSocket.close();
+    }
+  } 
+
+  }
+
