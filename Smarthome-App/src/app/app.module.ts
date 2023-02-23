6c5d72c82bb693c145b5340659b36da0da64c352
MaximilianHappe <63000113+MaximilianHappe@users.noreply.github.com>
2023-02-23 13:51:37 +0100

    Websocked Komponente anlegen

--- Smarthome-App/src/app/app.module.ts
+++ Smarthome-App/src/app/app.module.ts
@@ -44,6 +44,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { WebsocketComponent } from './websocket/websocket.component';
 
 
 
@@ -56,7 +57,8 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     MotorComponent,
     RoomComponent,
     LightRgbComponent,
-    LockComponent
+    LockComponent,
+    WebsocketComponent
   ],
   imports: [
     BrowserModule,
