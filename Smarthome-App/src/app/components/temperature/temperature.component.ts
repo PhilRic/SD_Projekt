7bc8d6a31dfd308853f401266ca40fb681c06ae7
PhilRic <73521963+PhilRic@users.noreply.github.com>
2023-02-27 00:43:51 +0100

    basic temp design

--- Smarthome-App/src/app/components/temperature/temperature.component.ts
+++ Smarthome-App/src/app/components/temperature/temperature.component.ts
@@ -8,8 +8,9 @@ import { WebSocketServiceService } from 'src/app/web-socket-service.service';
 export class TemperatureComponent implements OnInit, OnDestroy{
   @Input() device_id: string | undefined;
   @Input() name: string | undefined;
-  temperatur: number | undefined;
+  temperatur: number | undefined;// = 23;
   status: string = 'unknown';
+  backgroundTemp: string = 'lightgrey';
   
 
   constructor(private webSocketService: WebSocketServiceService) {}
@@ -18,12 +19,34 @@ export class TemperatureComponent implements OnInit, OnDestroy{
     const host = 'ws://raspberrypi.fritz.box:1880/ws/simple';
     this.webSocketService.connect(host);
     this.status = this.webSocketService.status;
+    this.changeBackground();                        // remove later
     this.webSocketService.onMessage((data) => {
       console.log('received', data);
       if (data.topic === this.device_id) {
         this.temperatur = data.payload;
+        this.changeBackground();
       } 
     });
+  }
+
+  changeBackground() {
+    if (this.temperatur != undefined) {
+      if (this.temperatur > 25) {
+        this.backgroundTemp = 'darkred';
+      }
+      else if (this.temperatur > 23 && this.temperatur <= 25) {
+        this.backgroundTemp = 'orange';
+      }
+      else if (this.temperatur > 21 && this.temperatur <= 23) {
+        this.backgroundTemp = 'yellow';
+      }
+      else if (this.temperatur > 19 && this.temperatur <= 21) {
+        this.backgroundTemp = '#0a38c4';
+      }
+      else if (this.temperatur <= 19) {
+        this.backgroundTemp = 'darkblue';
+      }
+    }
   }
 
   ngOnDestroy() {
