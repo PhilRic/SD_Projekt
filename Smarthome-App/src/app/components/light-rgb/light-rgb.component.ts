3a3b19c2827b7fe11749ae16b72da2ed27fe3a9d
PhilRic <73521963+PhilRic@users.noreply.github.com>
2023-02-28 21:13:16 +0100

    started rgb light design

--- Smarthome-App/src/app/components/light-rgb/light-rgb.component.ts
+++ Smarthome-App/src/app/components/light-rgb/light-rgb.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WebSocketServiceService } from 'src/app/web-socket-service.service';
 
 @Component({
   selector: 'app-light-rgb',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LightRgbComponent implements OnInit {
 
-  constructor() { }
+  @Input() device_id: string | undefined;
+  @Input() name: string | undefined;
+  status: string = 'unknown';
+  lamp_is_checked = false;
+  background: string = 'lightgrey';
+
+  constructor(private webSocketService: WebSocketServiceService) { }
 
   ngOnInit(): void {
+    const host = '';
+    this.webSocketService.connect(host);
+    this.status = this.webSocketService.status;
+    this.webSocketService.onMessage((data) => {
+      console.log('received', data);
+      if (data.topic === this.device_id) {
+        this.lamp_is_checked = data.payload;
+      } 
+    });
+  }
+
+  ngOnDestroy() {
+    this.webSocketService.close();
   }
 
 }
