31187f1c71c363a22192acdbfac5cae7ca0a72b4
MaximilianHappe <63000113+MaximilianHappe@users.noreply.github.com>
2023-03-05 21:10:30 +0100

    Firebase Implementation

---
+++ Smarthome-App/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  
+  
+}
